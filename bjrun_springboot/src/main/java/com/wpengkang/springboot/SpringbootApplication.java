a8152629e564de9958c6b571f96bed2eb68c5d27
wpengkang <wpengkang@DESKTOP-BACE4UD>
2018-11-04 18:46:13 +0800

    写注释

--- bjrun_springboot/src/main/java/com/wpengkang/springboot/SpringbootApplication.java
+++ bjrun_springboot/src/main/java/com/wpengkang/springboot/SpringbootApplication.java
@@ -3,7 +3,11 @@ package com.wpengkang.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 入口程序
+ * @author wpengkang
+ *
+ */
 @SpringBootApplication
 @MapperScan(basePackages="com.wpengkang.springboot.dao")
 public class SpringbootApplication {
